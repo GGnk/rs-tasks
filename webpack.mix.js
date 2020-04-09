74bcbf82b788c5b3ed6ba1447441bd9a4a82db22
GGnk <super-nurlan94@yandex.ru>
2020-04-09 15:27:59 +0400

    init: start task

---
+++ webpack.mix.js
@@ -0,0 +1,13 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.sass$/,
+                use: [
+                    'sass-loader'
+                ]
+            }
+        ]
+    }
+}
+
