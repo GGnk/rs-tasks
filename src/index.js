465d2473c6d4b4426ca6b10ca3e0ec9e62233b8d
GGnk <super-nurlan94@yandex.ru>
2020-04-11 20:58:00 +0400

    refactor: change the project structure, delete and add files.

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import config from "./js/store/config"
+import routes from "./js/store/routes"
---
+++ src/js/store/config.js
@@ -0,0 +1,12 @@
+let actions = {}
+let mutations = {}
+let getters = {}
+let state = {
+    home: true
+}
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+}
