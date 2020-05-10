7e7705423fed2e2de7ac0b4615713d70185fda75
Nurlan Kurbandurdyev <Super-nurlan94@yandex.ru>
2020-05-10 20:01:58 +0400

    feat: add function loadingHtml

--- movie-search/src/js/Movie.js
+++ movie-search/src/js/Movie.js
@@ -1,12 +1,13 @@
 export default class Movie {
   constructor(api, swiper) {
     this.start(api, swiper);
-  };
+  }
   start(api, swiper) {
     if(!api || !swiper) return console.log('Install the key OMDb Api and class Swiper !');
     this.api = api;
     this.swiper = swiper;
-  };
+    this.loading = document.querySelector('.loading');
+  }
 
   search(word, page = 1, loader = false) {
     if (!this.api) return console.log('Install the key!');
@@ -17,7 +18,7 @@ export default class Movie {
 
       this.getMovie(item, page)
         .then((items) => {
-          //console.log(items)
+
           let local = {
             word: item,
             countMovies: items.totalResults,
@@ -29,14 +30,15 @@ export default class Movie {
           return loader ? this.checkMoviesForLoader(items) : this.checkMovies(items);
         })
         .then((movies)=> {
-          if (!movies) return movies
+          if (!movies) return movies;
+
           movies.forEach((movie) => {
             this.getRating(movie).then(r => this.swiper.appendSlide(this.card(r)));
           });
         });
     });
 
-  };
+  }
 
   // Movie card template
   card(state) {
@@ -44,9 +46,20 @@ export default class Movie {
     return `<div class="swiper-slide">
         <input style="display: none" type="text" value="${state.imdbID}">
         <div class="card mb-4" style="border: none">
-          <div class="card-name">${state.Title}</div>
-          <img src="${(!state.Poster || state.Poster === 'N/A')? noPoster:state.Poster}" style="height: 400px" class="card-img-top" alt="${state.Title}">
+          <a href="https://www.imdb.com/title/${state.imdbID}" target="_blank" title="You will go to the page ${state.Type} '${state.Title}' in IMDb">
+            <div class="card-name">${state.Title}</div>
+            <img src="${(!state.Poster || state.Poster === 'N/A')? noPoster:state.Poster}" style="height: 400px" class="card-img-top" alt="${state.Title}">
+          </a>
           <div class="card-body">
+            <p class="card-text">
+                <span><u>Country</u>: ${state.Country}</span> <br>
+                <span><u>Released</u>: ${state.Released}</span><br>
+                <span><u>Genre</u>: ${state.Genre}</span><br>
+                <span><u>Language</u>: ${state.Language}</span><br>
+                <span><u>Awards</u>: ${state.Awards}</span><br>
+                <span><u>Actors</u>: ${state.Actors}</span><br>
+                <span><u>Plot</u>: ${state.Plot}</span><br>
+            </p>
             <div class="d-flex justify-content-between align-items-center">
               <small class="text-info">
                 ${state.Year} year
@@ -63,23 +76,26 @@ export default class Movie {
           </div>
         </div>
       </div>`;
-  };
+  }
 
   // Search for a movie
   async getMovie(search, page = 1) {
+    this.loadingHtml(true);
     const url = `https://www.omdbapi.com/?s=${search}&page=${page}&apikey=${this.api}`;
 
     const res = await fetch(url).catch(console.log.bind(console));
+    this.loadingHtml();
     return await res.json();
-  };
+  }
 
   // Getting a rating
   async getRating(movie) {
     const url = `https://www.omdbapi.com/?i=${movie.imdbID}&apikey=${this.api}`;
 
     const res = await fetch(url).catch(console.log.bind(console));
+    this.loadingHtml();
     return await res.json();
-  };
+  }
 
   // Checking the list for empty space
   checkMovies(list) {
@@ -113,9 +129,14 @@ export default class Movie {
 
   loader() {
     let local = this.getLocalStorage();
-    let promise = new Promise((resolve, reject) => {
+    let promise = new Promise((resolve) => {
       resolve(local.word);
     });
     this.search(promise, ++local.page, true);
+  }
+
+  loadingHtml(on = false) {
+    if (on) return this.loading.classList.add('active');
+    this.loading.classList.remove('active');
   }
 }
