99fe390644fc3c9b4564333d8bb248bdf5c21814
GGnk <super-nurlan94@yandex.ru>
2020-05-30 20:28:45 +0400

    fix: add method if

--- fancy-weather/src/components/blocks/MapBlock.vue
+++ fancy-weather/src/components/blocks/MapBlock.vue
@@ -2,10 +2,11 @@
   <div class="map-cluster">
     <div class="map-cluster__map">
       <iframe class="map-iframe"
+              v-if="getGeolocation"
               :src="`https://www.google.com/maps/embed/v1/view?center=${getGeolocation.geometry.lat},${getGeolocation.geometry.lng}&amp;zoom=10&amp;key=AIzaSyBWWZnqHV3asW7DM3yCQ0dxSHjj_J9LkwE&amp;language=en`"></iframe>
     </div>
-    <div class="map-cluster__coordinates"><p>Latitude: {{getGeolocation.geometry.lat.toFixed(2)}}</p>
-      <p>Longitude: {{getGeolocation.geometry.lat.toFixed(2)}}</p></div>
+    <div class="map-cluster__coordinates"><p>Latitude: {{getGeolocation? getGeolocation.geometry.lat.toFixed(2): ''}}</p>
+      <p>Longitude: {{getGeolocation? getGeolocation.geometry.lat.toFixed(2): ''}}</p></div>
   </div>
 </template>
 
