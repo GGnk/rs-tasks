84533750e3890efcdb4ecf6101a829ca8ac89c9a
GGnk <super-nurlan94@yandex.ru>
2020-06-03 21:43:49 +0400

    init: start task

--- fancy-weather/src/App.vue
+++ fancy-weather/src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderComponent/>
+    <WrapperComponent/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from '@/components/HeaderComponent';
+import WrapperComponent from '@/components/WrapperComponent';
+import {mapActions, mapGetters, mapMutations} from "vuex";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HeaderComponent,
+    WrapperComponent
+  },
+  data () {
+    return {
+
+      labels: ['SU', 'MO', 'TU', 'WED', 'TH', 'FR', 'SA'],
+      time: 0,
+      forecast: [
+        { day: 'Tuesday', icon: 'mdi-white-balance-sunny', temp: '24\xB0/12\xB0' },
+        { day: 'Wednesday', icon: 'mdi-white-balance-sunny', temp: '22\xB0/14\xB0' },
+        { day: 'Thursday', icon: 'mdi-cloud', temp: '25\xB0/15\xB0' },
+      ],
+    }
+  },
+  computed: {
+    ...mapGetters(['getGolocation'])
+
+  },
+  created() {
+    this.GET_LOCALSTORAGE()
+    this.SET_INFO_GEOLOCATION()
+  },
+  mounted() {
+    this.GET_DEFAULT_CITY()
+  },
+  methods: {
+    ...mapMutations(['GET_LOCALSTORAGE']),
+    ...mapActions(['SET_INFO_GEOLOCATION', 'GET_DEFAULT_CITY']),
+
+  },
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- fancy-weather/src/main.js
+++ fancy-weather/src/main.js
@@ -1,8 +1,16 @@
+import '@/main.scss'
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import Notifications from 'vue-notification'
+import i18n from './i18n'
+
+Vue.use(Notifications)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  i18n,
+  render: h => h(App)
 }).$mount('#app')
